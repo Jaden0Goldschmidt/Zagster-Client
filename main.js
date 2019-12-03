5565c1e7b2fe29ce291419a546ebf9adb0616649
Jaden Goldschmidt <goldschj@oregonstate.edu>
2019-12-03 10:38:37 -0800

    Cleaned comments, and changed titles.

--- main.js
+++ main.js
@@ -33,34 +33,34 @@ const BASE_URL = "https://zagster-service.herokuapp.com"
 //     options: {}
 // });
 // }
-$(updateView)
+// $(updateView)
 
-let years = []
-let months2016 = []
-let months2017 = []
-let months2018 = []
+// let years = []
+// let months2016 = []
+// let months2017 = []
+// let months2018 = []
 
 
-function updateView() { 
-  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
-  $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear), 
-  ).then(updateChart);
-}
+// function updateView() { 
+//   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+//   $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear), 
+//   ).then(updateChart);
+// }
 
-function perYear(data) {
+// function perYear(data) {
 
-  for (varindex = 0, month=9, index<= 3, month<=12; ++index, ++month){
-    months2016.push(data[2016] [index] [month])
-  }
-  console.log("2016 data by months is easy" + months2016)
+//   for (varindex = 0, month=9, index<= 3, month<=12; ++index, ++month){
+//     months2016.push(data[2016] [index] [month])
+//   }
+//   console.log("2016 data by months is easy" + months2016)
 
-  for (varindex = 0, month=1, index<= 11, month<=12; ++index, ++month){
-    months2017.push(data[2017] [index] [month])
-  }
-  console.log("2017 data by months is easy" + months2016)
+//   for (varindex = 0, month=1, index<= 11, month<=12; ++index, ++month){
+//     months2017.push(data[2017] [index] [month])
+//   }
+//   console.log("2017 data by months is easy" + months2016)
 
-  for (varindex = 0, month=1, index<= data.length, month<=12; ++index, ++month){
-    months2018.push(data[2018] [index] [month])
-  }
-  console.log("2018 data by months is easy" + months2018)
+//   for (varindex = 0, month=1, index<= data.length, month<=12; ++index, ++month){
+//     months2018.push(data[2018] [index] [month])
+//   }
+//   console.log("2018 data by months is easy" + months2018)
 }
